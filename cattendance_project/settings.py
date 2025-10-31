f05b8af2334b9b1d1fbd3e183d3b666b60cfa6a4
ItzAaliyahh20 <maturanfrances123@gmail.com>
2025-10-31 13:40:33 +0800

    dashboars_app: manage_class(ui improvement)

--- cattendance_project/settings.py
+++ cattendance_project/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
+ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', 'csit327-g1-cattendance.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'https://csit327-g1-cattendance.onrender.com']
 
 
